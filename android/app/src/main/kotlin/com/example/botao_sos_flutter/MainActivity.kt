f6f96771efb922eed380f3eb11334b3250fe0f93
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-12-22 19:20:04 -0300

    first push

---
+++ android/app/src/main/kotlin/com/example/botao_sos_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.botao_sos_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+main() => runApp(
+      ButtonApp(),
+    );
+
+class ButtonApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<http.Response> _postEvent() async {
+    final data = {
+      "event": {"iduff": "iduff"}
+    };
+    final http.Response response = await http.post(
+      Uri.parse('http://localhost:3000/events'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(data),
+    );
+    return response;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).errorColor,
+        title: Text("Botão SOS"),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Image.asset("assets/images/BotãoSOS-Logo.png"),
+              height: 60,
+              margin: EdgeInsets.all(20),
+            ),
+            SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: _postEvent,
+              child: Icon(
+                Icons.sos,
+                size: 150,
+              ),
+              style: ButtonStyle(
+                shape: MaterialStateProperty.all(CircleBorder()),
+                padding: MaterialStateProperty.all(EdgeInsets.all(20)),
+                backgroundColor: MaterialStateProperty.all(
+                    Theme.of(context).errorColor), // <-- Button color
+                overlayColor:
+                    MaterialStateProperty.resolveWith<Color?>((states) {
+                  if (states.contains(MaterialState.pressed))
+                    return Colors.red; // <-- Splash color
+                }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
